345a66d497dd47e93ff6bf39363121c62901b8b0
Fernando Miranda <fernando@projeteweb.com.br>
2015-05-13 16:04:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# jQuery Truncate
+
+Simple text truncate with options by show full text in templates
+
+## Usage
+
+HTML:
+
+```HTML
+<h3 class="element">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h3>
+```
+
+JavaScript:
+
+```JavaScript
+$('.element').truncate({
+	length: 50,
+    limit: -1,
+    style: 'bootstrapPopover',
+    trigger: 'hover', // hover | click
+    trailing: '...',
+    placement: 'auto' // top | bottom | left | right | auto
+});
+```
+
+Output:
+```
+Lorem ipsum dolor sit amet, consectetur adipisicin...
+```
+
+## Requirements
+* jQuery 1.7+
+
+## License
+Copyright (c) 2015 [Fernando Miranda](fernando@projeteweb.com.br), released under the MIT License.
---
+++ dist/jquery.truncate.js
@@ -0,0 +1,52 @@
+/*!
+ * Truncate - jQuery Plugin
+ * version: 0.8.0
+ * @requires jQuery v1.7 or later
+ *
+ * Copyright 2015 Fernando Miranda - fernando@projeteweb.com.br
+ *
+ */
+
+(function ($) {
+    $.fn.truncate = function(options) {
+
+        var settings = $.extend({
+            length: 50,
+            limit: -1,
+            style: 'bootstrapPopover',
+            trigger: 'hover', // hover | click
+            trailing: '...',
+            placement: 'auto' // top | bottom | left | right | auto
+        }, options);
+
+        return this.each(function() {
+        	var that = $(this),
+        		text = $.trim(that.text()),
+        		length = text.length;
+
+        	if (length > settings.length) {
+        		var arr = text.split('', settings.length),
+        			textTruncated = arr.join('') + settings.trailing;
+
+        		if (settings.limit > -1) {
+        			text = text.split('', settings.limit).join('');
+        		}
+
+        		that.addClass('text-truncated').text(textTruncated).data('text-original', text);
+
+        		switch (settings.style) {
+        		case 'bootstrapPopover':
+        			$(that).popover({
+        				trigger: settings.trigger,
+        				content: text,
+        				placement: settings.placement
+        			});
+        			break;
+        		default:
+        			break;
+        		}
+        	}
+        });
+
+    };
+}(jQuery));
